993815df0564edba8b9630b25fcdb78d0e122db7
envagogit <envagogit@gmail.com>
2023-05-14 00:27:47 +0200

    Onion

--- birras_y_tacos.py
+++ birras_y_tacos.py
@@ -1,12 +1,19 @@
 import os
 import streamlit as st
+import time
+import streamlit.components.v1 as components
+import re
 
 apikey = st.secrets["OPENAI_API_KEY"]
+propmpt_template1 = st.secrets["prompt_template1"]
+propmpt_template2 = st.secrets["prompt_template2"]
+fake_answer = st.secrets["fake_answer"]
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
 from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper
+from langchain.callbacks import get_openai_callback
 
 ##### REMEMBER TO SAVEEE! #####
 
@@ -16,85 +23,111 @@ os.environ[
 
 # pip install streamlit langchain openai wikipedia chromadb tiktoken
 
-# TABS
-tab1, tab2 = st.tabs(["Paco", "Menu Completo"])
+###### Hide Menu
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+# add_selectbox = st.sidebar.selectbox(
+#        "How would you like to be contacted?", ("Email", "Home phone", "Mobile phone")
+#    )
 
+####### TABS
+tab1, tab2 = st.tabs(["Sugeridor", "Menu Completo"])
+
+####### TAB1
 with tab1:
     # App framework
-    st.title("🍻 ¡Birras y Tacos! 🌮")
-    st.header("Chatea con Paco, nuestro sugeridor favorito")
-    st.divider()
+    prompt_is_filled = False
+    st.subheader("🍻 ¡Birras y Tacos! 🌮", anchor=False)
 
-    add_selectbox = st.sidebar.selectbox(
-        "How would you like to be contacted?", ("Email", "Home phone", "Mobile phone")
-    )
-    col1, col2 = st.columns(2)
+    ###### Customer Inputs
+    col1, col2 = st.columns([3, 1])
+
     with col1:
-        prompt = st.text_input("¿Qué te apetece hoy 😏?")
-        st.write("Puedes probar cosas como: ")
-        st.write("· delicioso y vegetariano ")
-        st.write("· algo ligero ")
-        st.write("· algo con tomate y queso ")
-        st.write(". algo de 6 euros o menos ")
-
-    with col2:
-        st.image(
-            "https://images.app.goo.gl/FHo81MaycGWWuJSC8", "Nuestros deliciosos tacos"
+        prompt = st.text_input(
+            "¿Qué te apetece hoy?",
+            max_chars=80,
+            placeholder="Algo fresco",
+            label_visibility="collapsed",
         )
-    # Prompt templates
+    ###### Number of people
+    with col2:
+        num_people = st.selectbox(
+            "¿Para cuántas personas?",
+            (
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                "10+",
+                "Núm. Personas",
+            ),
+            label_visibility="collapsed",
+            index=10,
+        )
+    if num_people == 1:
+        propmpt_num_people = ""
+    else:
+        propmpt_num_people = ". Para " + num_people + "personas"
+
+    search = st.button("**¡Pregunta a nuestro sugeridor!**", type="secondary")
+    if prompt_is_filled == False & search == False:
+        st.divider()
+        st.write("Puedes preguntar cosas como: ")
+        col1, col2 = st.columns(2)
+        with col1:
+            st.button("Algo delicioso y vegetariano ")
+            st.button("Algo ligero ")
+            st.button("Una cerveza de trigo y de importación ")
+        with col2:
+            st.button("Algo con tomate y queso ")
+            st.button("Algo de 6 euros o menos ")
+            st.button("Algo para compartir ")
+
+    ###### Prompt templates
     title_template = PromptTemplate(
         input_variables=["user_input"],
-        template="You are Paco, the best waiter of Beers and tacos. \n \
-        You must answer the customers only if the query is related to the restaurant, otherwise just refer them to Mery. \n \
-        This is the menu: \n \
-            Classic Beef Tacos (7.99€): seasoned ground beef, shredded cheese, lettuce, tomato, sour cream, and salsa served in a warm tortilla \n \
-            Grilled Chicken Tacos (6.99€): marinated grilled chicken, avocado, pico de gallo, and queso fresco served in a warm tortilla \n \
-            Baja Fish Tacos (9.99€): beer-battered cod, cabbage slaw, chipotle mayo, and lime served in a warm tortilla \n \
-            Veggie Tacos (5.99€): roasted sweet potato, black beans, corn, avocado, and cilantro served in a warm tortilla \n \
-            Loaded Nachos(10.99€): crispy tortilla chips topped with seasoned ground beef, queso cheese, jalapeños, tomatoes, black olives, and sour cream \n \
-            Quesadilla (7.49€): melted cheddar and jack cheese, grilled chicken or steak, sautéed onions and peppers, served with salsa and sour cream \n \
-            Char-Grilled Burger(9.49€): 1/3 lb beef patty, lettuce, tomato, pickles, and special sauce on a brioche bun \n \
-            Chicken Wings (5.49€): choice of buffalo, honey BBQ, or garlic parmesan served with celery and ranch or blue cheese dressing \n \
-            Beer-battered Onion Rings(3.49€): crispy onion rings served with chipotle mayo dipping sauce \n \
-            Fried Avocado Bites (2.49€): breaded and fried avocado wedges served with ranch dipping sauce \n \
-        Remember to: \n \
-            1. be polite and to also include pricing.\n \
-            2. include pricing using € \n \
-            3. give a detailed description of the dish \n \
-            4. explain why do you recommend it \n \
-            5. give an alternative \n \
-            6. divide each suggestion into a different paragraph \n \
-        This is the query of the customer: {user_input}",
+        template=propmpt_template1
+        + "Esta es la petición del cliente: {user_input}"  # "This is the query of the customer:
+        + propmpt_template2
+        + propmpt_num_people,
     )
-
     script_template = PromptTemplate(
-        input_variables=["title", "restaurant_menu"],
-        template="write me a youtube video script based on this title. Title: {title}.  You can leverage this wikipedia research: {restaurant_menu}, as long as your answer has the format of a script",
+        input_variables=["title"],
+        template="Translate the following text to Spanish without adding any extra text :{title}",
     )
     director_template = PromptTemplate(
         input_variables=["title", "director"],
         template="Add a very short paragraph explaining why {director} is perfect for the presenter role for the video titled {title}",
     )
-    # Memory
+    ###### Memory
     title_memory = ConversationBufferMemory(
         input_key="user_input", memory_key="chat_history"
     )
     script_memory = ConversationBufferMemory(
         input_key="title", memory_key="chat_history"
     )
-    # Llms
+    ###### Llms
     llm = OpenAI(temperature=0.4)
     title_chain = LLMChain(
         llm=llm,
         prompt=title_template,
-        verbose=True,
+        verbose=False,
         output_key="title",
         memory=title_memory,
     )
     script_chain = LLMChain(
         llm=llm,
         prompt=script_template,
-        verbose=True,
+        verbose=False,
         output_key="script",
         memory=script_memory,
     )
@@ -105,17 +138,55 @@ with tab1:
         output_key="director_script",
     )
     wiki = WikipediaAPIWrapper()
-    # show stuff if there is a prompt
+    ###### show stuff if there is a prompt
     if prompt:
+        prompt_is_filled = True
+    if search & prompt_is_filled:
+        title = fake_answer
+        print(prompt)
+        # with get_openai_callback() as cb:
+        #    title = title_chain.run(prompt)
+        # script = script_chain.run(title=title)
+        #    print(cb)
 
-        title = title_chain.run(prompt)
+        ###### Progress bar
+
+        progress_text = "Preguntando a Paco...😋"
+        my_bar = st.progress(0, text=progress_text)
+        st.divider()
+        for percent_complete in range(100):
+            time.sleep(0.01)
+            progress_text = f"Preguntando a Paco... {percent_complete+1}% 😋"
+            my_bar.progress(percent_complete + 1, text=progress_text)
+
         # wiki_research = wiki.run(prompt)
         # script = script_chain.run(title=title, restaurant_menu=wiki_research)
         # director = director_chain.run(title=title, director="Enrique")
-        print(title)
-        st.divider()
-        st.write(title)
-        st.success("Success message!", icon="✅")
+
+        ###### Format Output
+        answer = title.replace(";", ".")
+        answer = answer.replace("[", "**")
+        answer = answer.replace("]", "**")
+        if answer[0:2] == ", ":
+            answer = answer.replace(", ", "", 1)
+
+        answer = answer.capitalize()
+        phrases = answer.split(sep=".")
+        print(phrases)
+        answer = ""
+        for phrase in phrases:
+            answer = answer + ". " + phrase.strip().capitalize()
+        answer = answer.replace(". ", "", 1)
+        answer = answer.replace(". ", ". <br>")
+        print(answer)
+        st.markdown(answer, unsafe_allow_html=True)
+
+        ###### Create Expanders
+        recomendations = re.findall(r"\[.*?\]", title)
+
+        with st.expander("Recomendaciones"):
+            st.write(recomendations)
+
         # st.write(response["title"])
         # st.write(script)
         # st.write(director)
@@ -127,3 +198,16 @@ with tab1:
 
         # with st.expander("Wikipedia Research History"):
         # st.info(wiki_research)
+
+
+###### TAB 2
+with tab2:
+    st.components.v1.iframe(
+        src="https://drive.google.com/uc?export=view&id=1hmwTFKwuLfBJm7kAy92EAxtCKg0y0nOS",
+        width=900,
+        height=900,
+    )
+    # st.image(
+    #    "https://drive.google.com/file/d/1hmwTFKwuLfBJm7kAy92EAxtCKg0y0nOS/preview",
+    #    "Nuestros deliciosos tacos",
+    # )
