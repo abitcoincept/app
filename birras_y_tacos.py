1473faa599c3d1f969b06bc26b43ba84288fae65
envagogit <envagogit@gmail.com>
2023-05-14 20:20:43 +0200

    Onion7

--- birras_y_tacos.py
+++ birras_y_tacos.py
@@ -4,11 +4,15 @@ import time
 import streamlit.components.v1 as components
 import re
 
+from streamlit_extras.card import card
+from streamlit_extras.badges import badge
+from streamlit_extras.metric_cards import style_metric_cards
+
 apikey = st.secrets["OPENAI_API_KEY"]
 propmpt_template1 = st.secrets["prompt_template1"]
 propmpt_template2 = st.secrets["prompt_template2"]
 fake_answer = st.secrets["fake_answer"]
-# food_images = st.secrets["food_images"]
+
 
 use_fake_prompt = True
 
@@ -19,18 +23,21 @@ from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper
 from langchain.callbacks import get_openai_callback
 
+##### Card Title
+
+
 ##### Dictionaries
 food_images = {
-    "tacos de ternera clásicos": "https://drive.google.com/uc?export=view&id=17RzYwLWvEIYy99AQ064guCIWx7XgjkrX",
-    "tacos de pollo a la parrilla": "https://drive.google.com/uc?export=view&id=17RzYwLWvEIYy99AQ064guCIWx7XgjkrX",
-    "baja fish tacos": "https://drive.google.com/uc?export=view&id=17RzYwLWvEIYy99AQ064guCIWx7XgjkrX",
-    "veggie tacos": "https://drive.google.com/uc?export=view&id=17RzYwLWvEIYy99AQ064guCIWx7XgjkrX",
-    "nachos cargados": "https://drive.google.com/uc?export=view&id=1OsX--f4iCrWcluxLmCWQ628HpJpwqBmI",
-    "quesadilla": "https://drive.google.com/uc?export=view&id=1CVK6ZiKQbEV1Ev4C5COe9QI90EcBD42t",
-    "hamburguesa a la brasa": "https://drive.google.com/uc?export=view&id=1SGA2ccyiuaiDwUoHtrq-EPTWJ1n6C6gv",
-    "alitas de pollo": "https://drive.google.com/uc?export=view&id=12SystKdDfoNBvu6PE7j88Uvy2cOWTjzd",
-    "aros de cebolla rebozados con cerveza": "https://drive.google.com/uc?export=view&id=1HIofkJJkBiik4Jvs86WhGEgRjEPBB_8q",
-    "fried avocado bites": "https://drive.google.com/uc?export=view&id=1D87paeSaif7pw9jtT5ZoRLnwdmbleyY_",
+    "tacos de ternera clásicos": "https://i.ibb.co/84wKHLc/Receita-de-tacos-mexicanos-150x150.png",
+    "tacos de pollo a la parrilla": "https://i.ibb.co/84wKHLc/Receita-de-tacos-mexicanos-150x150.png",
+    "baja fish tacos": "https://i.ibb.co/84wKHLc/Receita-de-tacos-mexicanos-150x150.png",
+    "veggie tacos": "https://i.ibb.co/84wKHLc/Receita-de-tacos-mexicanos-150x150.png",
+    "nachos cargados": "https://i.ibb.co/vHRW7Yk/SOFT-BEEF-TACOS-770x628-150x150.jpg",
+    "quesadilla": "https://i.ibb.co/r4h76Ld/breakfast-quesadilla-150x150.jpg",
+    "hamburguesa a la brasa": "https://i.ibb.co/MRQv5qQ/meatless-chipotle-pesto-burger-3-150x150.jpg",
+    "alitas de pollo": "https://i.ibb.co/KNPdqkq/alitas-de-pollo-teriyaki-150x150.jpg",
+    "aros de cebolla rebozados con cerveza": "https://i.ibb.co/jV5jk9J/conoce-la-receta-de-los-aros-de-cebolla-y-cautiva-a-tus-invitados-150x150.jpg",
+    "fried avocado bites": "https://i.ibb.co/R9mrVhZ/Air-Fryer-Fried-Avocado-Picture-150x150.png",
 }
 food_prices = {
     "tacos de ternera clásicos": "7,99€",
@@ -52,20 +59,56 @@ food_prices = {
     "founders breakfast stout": "4€",
     "westvleteren 12": "6€",
 }
+food_descriptions = {
+    "tacos de ternera clásicos": "carne molida sazonada, queso rallado, lechuga, tomate, nata y salsa servidos en una tortilla caliente",
+    "tacos de pollo a la parrilla": "pollo a la parrilla marinado, aguacate, pico de gallo y queso fresco servido en una tortilla calentita",
+    "baja fish tacos": "bacalao rebozado con cerveza, ensalada de col, mayonesa de chipotle y lima servido en una tortilla caliente",
+    "veggie tacos": "batata asada, frijoles negros, maíz, aguacate y cilantro servidos en una tortilla caliente",
+    "nachos cargados": "chips de tortilla crujientes cubiertos con carne molida sazonada, queso, jalapeños, tomates, aceitunas negras y crema agria",
+    "quesadilla": "queso cheddar y jack derretido, pollo o bistec a la parrilla, cebollas y pimientos salteados, servida con salsa y crema agria",
+    "hamburguesa a la brasa": "1/3 lb de hamburguesa de ternera, lechuga, tomate, pepinillos y salsa especial sobre pan brioche",
+    "alitas de pollo": "salsa de búfalo, barbacoa de miel o parmesano con ajo, servidas con apio y aderezo ranch o queso azul",
+    "aros de cebolla rebozados con cerveza": "aros de cebolla crujientes servidos con salsa chipotle mayonesa",
+    "fried avocado bites": "gajos de aguacate rebozados y fritos servidos con salsa ranchera",
+    "chimay blue": "Cerveza belga con un color marrón intenso y un sabor rico y complejo. Tiene notas de frutos negros y especias, con un final ligeramente amargo.",
+    "weihenstephaner hefeweissbier": "Cerveza de trigo alemana es de color dorado claro y tiene un aspecto turbio debido a la levadura. Tiene un sabor refrescante y afrutado con toques de plátano y clavo.",
+    "hitachino nest white ale": "Cerveza japonesa se elabora con cilantro y nuez moscada, lo que le da un sabor picante y ligeramente dulce. Tiene un aroma ligero y cítrico y un final crujiente.",
+    "la chouffe": "Cerveza belga tiene un color dorado y un aroma afrutado con toques de pera y manzana. Tiene un sabor ligeramente dulce con un final seco.",
+    "rodenbach grand cru": "Cerveza agria belga tiene un color rojo intenso y un sabor agrio y afrutado. Es envejecido durante dos años en barricas de roble, lo que le da un perfil de sabor complejo.",
+    "augustiner helles": "Cerveza alemana es de color dorado pálido y tiene un sabor ligero y crujiente con un toque de dulzura de miel. Tiene un final limpio y refrescante.",
+    "founders breakfast stout": "Cerveza estadounidense se elabora con café y chocolate, lo que le da un sabor rico y cremoso. Tiene un fuerte aroma a café y un final ligeramente amargo.",
+    "westvleteren 12": "Cerveza belga es elaborada por monjes trapenses y es considerada una de las mejores cervezas del mundo. Tiene un color marrón oscuro y un sabor complejo con notas de caramelo, chocolate y frutos negros.",
+}
+food_types = {
+    "tacos de ternera clásicos": "principales",
+    "tacos de pollo a la parrilla": "principales",
+    "baja fish tacos": "principales",
+    "veggie tacos": "principales",
+    "nachos cargados": "entrantes",
+    "quesadilla": "entrantes",
+    "hamburguesa a la brasa": "entrantes",
+    "alitas de pollo": "entrantes",
+    "aros de cebolla rebozados con cerveza": "entrantes",
+    "fried avocado bites": "entrantes",
+    "chimay blue": "cervezas",
+    "weihenstephaner hefeweissbier": "cervezas",
+    "hitachino nest white ale": "cervezas",
+    "la chouffe": "cervezas",
+    "rodenbach grand cru": "cervezas",
+    "augustiner helles": "cervezas",
+    "founders breakfast stout": "cervezas",
+    "westvleteren 12": "cervezas",
+}
+logo_url = "https://i.ibb.co/84wKHLc/Receita-de-tacos-mexicanos-150x150.png"
+restaurant_name = "Cervezas y Tacos"
+
 add_to_cart_list = []
 remove_from_cart_list = []
 ##### Custom Functions
-# @st.cache_data(experimental_allow_widgets=True)
 def image_parser(answer, food_images):
     recomendations = re.findall(r"\[.*?\]", answer)
 
     i = 0
-    recomendations_aux = []
-    for dish in recomendations:
-        dish_aux_2 = dish.replace("[", "")
-        dish_aux_2 = dish_aux_2.replace("]", "")
-        dish_aux_2 = dish_aux_2.lower()
-        recomendations_aux.append(dish_aux_2)
 
     for dish in recomendations:
         dish_aux = dish.replace("[", "")
@@ -89,11 +132,7 @@ def image_parser(answer, food_images):
             if dish_aux in food_images:
                 col1, col2, col3, col4 = st.columns(4)
                 with col2:
-                    st.components.v1.iframe(
-                        src=food_images[dish_aux],
-                        width=150,
-                        height=150,
-                    )
+                    add_image(source=food_images[dish_aux])
                 with col3:
                     button_clicked = st.button(
                         ":white[Añadir a la lista: \n **"
@@ -101,10 +140,17 @@ def image_parser(answer, food_images):
                         + "** ("
                         + food_prices[dish_aux]
                         + ")]",
-                        type="secondary",
+                        type="primary",
                     )
+                    if dish_aux in st.session_state:
+                        st.caption(
+                            "**Añadidos: "
+                            + str(st.session_state[dish_aux])
+                            + "**  :heavy_check_mark:"
+                        )
                     if button_clicked == True:
                         add_to_cart_list.append(dish_aux)
+
             else:
                 col1, col2, col3 = st.columns(3)
                 with col2:
@@ -114,10 +160,17 @@ def image_parser(answer, food_images):
                         + "** ("
                         + food_prices[dish_aux]
                         + ")]",
-                        type="secondary",
+                        type="primary",
                     )
+                    if dish_aux in st.session_state:
+                        st.caption(
+                            "**Añadidos: "
+                            + str(st.session_state[dish_aux])
+                            + "**  :heavy_check_mark:"
+                        )
                     if button_clicked == True:
                         add_to_cart_list.append(dish_aux)
+
         answer = split_answer[1]
 
         i = i + 1
@@ -173,10 +226,10 @@ def format_answer(title):
     return answer
 
 
-def update_sidebar():
+def create_cart():
     add_to_cart_list = []
     remove_from_cart_list = []
-    with tab3:
+    with tab5:
         key = 0
         session_aux = st.session_state
         for items in session_aux:
@@ -200,7 +253,7 @@ def update_sidebar():
                     with col3:
                         button_clicked_rem = st.button(
                             label="**-**",
-                            type="secondary",
+                            type="primary",
                             key=key + 2,
                         )
                         if button_clicked_rem == True:
@@ -208,7 +261,7 @@ def update_sidebar():
                     with col4:
                         button_clicked_add = st.button(
                             label="**+**",
-                            type="secondary",
+                            type="primary",
                             key=key + 3,
                         )
                         if button_clicked_add == True:
@@ -217,6 +270,15 @@ def update_sidebar():
     add_remove_cart_items(
         add_to_cart_list=add_to_cart_list,
         remove_from_cart_list=remove_from_cart_list,
+    )
+
+
+@st.cache_data
+def add_image(source):
+    st.components.v1.iframe(
+        src=source,
+        width=150,
+        height=150,
     )
 
 
@@ -230,6 +292,70 @@ def run_llm(prompt, use_fake_prompt, fake_answer):
             # title = script_chain.run(title=title)
             print(cb)
     return title
+
+
+def full_menu(food_prices, food_images, type):
+    i = 0
+    for dish in food_prices:
+        if food_types[dish] == type:
+            dish_aux = dish.replace("[", "")
+            dish_aux = dish_aux.replace("]", "")
+            dish_aux = dish_aux.lower()
+            container = st.container()
+            with container:
+                button_clicked = False
+                if dish_aux in food_images:
+                    col1, col2 = st.columns([1, 3])
+                    with col1:
+                        add_image(source=food_images[dish_aux])
+                    with col2:
+                        button_clicked = st.button(
+                            "" + dish_aux.title() + " (" + food_prices[dish_aux] + ")",
+                            type="primary",
+                        )
+                        st.caption(food_descriptions[dish_aux].capitalize())
+                        if dish_aux in st.session_state:
+                            st.caption(
+                                "**Añadidos: "
+                                + str(st.session_state[dish_aux])
+                                + "**  :heavy_check_mark:"
+                            )
+
+                        if button_clicked == True:
+                            add_to_cart_list.append(dish_aux)
+
+                else:
+                    button_clicked = st.button(
+                        "" + dish_aux.title() + " (" + food_prices[dish_aux] + ")",
+                        type="primary",
+                    )
+                    st.caption(food_descriptions[dish_aux].capitalize())
+                    if dish_aux in st.session_state:
+                        st.caption(
+                            "**Añadidos: "
+                            + str(st.session_state[dish_aux])
+                            + "**  :heavy_check_mark:"
+                        )
+
+                    if button_clicked == True:
+                        add_to_cart_list.append(dish_aux)
+
+            # answer = split_answer[1]
+
+            i = i + 1
+
+    add_remove_cart_items(
+        add_to_cart_list=add_to_cart_list,
+        remove_from_cart_list=remove_from_cart_list,
+    )
+
+
+def add_card(title, image):
+    card(
+        title=title,
+        text="",
+        image=image,
+    )
 
 
 os.environ[
@@ -247,16 +373,40 @@ hide_menu_style = """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 ####### TABS
-tab1, tab2, tab3 = st.tabs(["Sugeridor", "Menu Completo", "Añadido a la lista"])
-
+items_in_list = 0
+for item in st.session_state:
+    if item in food_prices:
+        items_in_list = items_in_list + st.session_state[item]
+if items_in_list == 0:
+    tab1, tab2, tab3, tab4, tab5 = st.tabs(
+        [
+            "Sugerencias :cook:",
+            "Para Compartir :stew:",
+            "Tacos :taco:",
+            "Cervezas :beers:",
+            "Mi Lista :memo:",
+        ]
+    )
+else:
+    tab1, tab2, tab3, tab4, tab5 = st.tabs(
+        [
+            "Sugerencias :cook:",
+            "Para Compartir :stew:",
+            "Tacos :taco:",
+            "Cervezas :beers:",
+            "Mi Lista (**" + str(items_in_list) + "**) :memo:",
+        ]
+    )
 ####### TAB1
 with tab1:
     # App framework
+    add_card(image=logo_url, title=restaurant_name)
+
     prompt_is_filled = False
-    st.subheader("🍻 Birras y Tacos 🌮", anchor=False)
+    # st.subheader("🍻 Birras y Tacos 🌮", anchor=False)
 
     ###### Customer Inputs
-    col1, col2 = st.columns([3, 1])
+    col1, col2 = st.columns([3, 2])
 
     with col1:
         prompt = st.text_input(
@@ -267,30 +417,13 @@ with tab1:
         )
     ###### Number of people
     with col2:
-        num_people = st.selectbox(
-            "¿Para cuántas personas?",
-            (
-                "1",
-                "2",
-                "3",
-                "4",
-                "5",
-                "6",
-                "7",
-                "8",
-                "9",
-                "10+",
-                "Núm. Personas",
-            ),
-            label_visibility="collapsed",
-            index=10,
-        )
+        search = st.button("**¡Pregunta sobre nuestra carta!**", type="primary")
+    num_people = 1
     if num_people == 1:
         propmpt_num_people = ""
     else:
         propmpt_num_people = ". Para " + num_people + "personas"
 
-    search = st.button("**¡Pregunta a nuestro sugeridor!**", type="secondary")
     if prompt == "":
         ### Sugerencias preguntas
         st.write("Puedes preguntar cosas como: ")
@@ -368,16 +501,13 @@ with tab1:
         ###### Create Expanders
         recomendations = re.findall(r"\[.*?\]", title)
         ###### Side Bar
-    update_sidebar()
+    create_cart()
 
 ###### TAB 2
 with tab2:
-    st.components.v1.iframe(
-        src="https://drive.google.com/uc?export=view&id=1hmwTFKwuLfBJm7kAy92EAxtCKg0y0nOS",
-        width=900,
-        height=900,
-    )
-    # st.image(
-    #    "https://drive.google.com/file/d/1hmwTFKwuLfBJm7kAy92EAxtCKg0y0nOS/preview",
-    #    "Nuestros deliciosos tacos",
-    # )
+    full_menu(food_prices=food_prices, food_images=food_images, type="entrantes")
+with tab3:
+    full_menu(food_prices=food_prices, food_images=food_images, type="principales")
+with tab4:
+    full_menu(food_prices=food_prices, food_images=food_images, type="cervezas")
+badge(type="twitter", name=restaurant_name)
